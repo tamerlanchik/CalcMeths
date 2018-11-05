f35a88b8fb5672eed345a70801214e4a9dceff12
Andrey K <andreykochnov@yandex.ru>
2018-11-05 19:42:01 +0300

    Участок определения N перенесен в начало

--- HW1/hw_int.m
+++ HW1/hw_int.m
@@ -10,16 +10,10 @@ eps = 10^(-6);
 n = [10,100,1000];
 names = {'riemann_left', 'riemann_mid', 'riemann_right', 'trapezoidal', 'simpson', 'gaussian with 5 points'};
 S = zeros(length(n), length(names));
-for i=(1:1:3)
-   for j=(1:1:6)
-       ans = hw_int_analog(a,b,n(i), @hw_int_func, names{j});
-       S(i,j) = ans;
-   end
-end
 
 N=10;
 d2n=ones(1, length(names));
-In = S(1,:);
+In = zeros(1, length(names));
 I2n = zeros(1, length(names));
 while nnz(d2n>eps)~=0
    for j=(1:1:6)
@@ -35,10 +29,10 @@ while nnz(d2n>eps)~=0
    d2n(6) = d2n(1)/15;
    N=N*2;
 end
-n = [n,N];
-for i=(1:1:length(n))
-   for j=(1:1:length(names))
-       ans = hw_int_analog(a,b,n(i), @hw_int_func, names{j})
+n=[10,100,1000,N];
+for i=(1:1:3)
+   for j=(1:1:6)
+       ans = hw_int_analog(a,b,n(i), @hw_int_func, names{j});
        S(i,j) = ans;
    end
 end
