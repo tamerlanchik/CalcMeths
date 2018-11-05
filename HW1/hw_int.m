c5431c94ab8737a03752e12619a467298bed16e8
Andrey K <andreykochnov@yandex.ru>
2018-11-05 20:59:21 +0300

    Добавлены графики

--- HW1/hw_int.m
+++ HW1/hw_int.m
@@ -7,10 +7,11 @@ addpath("./algos");
 a = 0.2;
 b = 0.7;
 eps = 10^(-6);
-names = {'riemann_left', 'riemann_mid', 'riemann_right', 'trapezoidal', 'simpson', 'gaussian with 5 points'};
+names = {'riemann_left', 'riemann_mid', 'riemann_right', 'trapezoidal', 'simpson', 'gaussian_with_5_points'};
 
 N=10;
 d2n=ones(1, length(names));
+D2n=[];
 In = zeros(1, length(names));
 I2n = zeros(1, length(names));
 while nnz(d2n>eps)~=0
@@ -25,6 +26,7 @@ while nnz(d2n>eps)~=0
    d2n(4) = d2n(1)/3;
    d2n(5) = d2n(1)/15;
    d2n(6) = d2n(1)/15;
+   D2n = [D2n; d2n];
    N=N*2;
 end
 n=[10,100,1000,N];
@@ -35,4 +37,49 @@ for i=(1:1:length(n))
        S(i,j) = ans;
    end
 end
-S=S
+
+disp(array2table(S, 'VariableNames', names));
+fprintf('N = %d\n', n(length(n)));
+
+%-----------------------------------
+
+clf;
+figure(1);
+set(gcf, 'Position', [50, 200, 1500, 600]);
+
+subplot(2,2,1);
+t=(a:(b-a)/N:b)';
+s1=hw_int_func(t);
+plot(t, s1);
+grid on;
+grid minor;
+xlabel('X');
+ylabel('Y\omega');
+hold on;
+
+subplot(2,2,3);
+bar(S, 'Hist');
+ylim([0.214, 0.221]);
+ylabel('Value');
+xlabel('N');
+Ns=cell(1,length(n));
+for i=(1:1:length(n))
+    Ns{i} = num2str(n(i));
+end
+xticklabels(Ns);
+grid on;
+grid minor;
+
+subplot(2,2,[2,4]);
+b=bar(D2n, 'FaceColor', 'flat');
+grid on;
+grid minor;
+ylim([0, 0.00001]);
+for k = 1:size(D2n,2)
+    b(k).CData = k;
+end
+ylim([0, 0.00001])
+
+xlabel('Number of iteration');
+ylabel(texlabel('Delta2n'));
+legend(strrep(names, '_', ' '));
