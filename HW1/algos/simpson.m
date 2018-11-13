65807cee1afd5fa2d391f0656ebabf8de11f4cdf
Andrey K <andreykochnov@yandex.ru>
2018-11-13 21:50:42 +0300

    Update simpson.m

--- HW1/algos/simpson.m
+++ HW1/algos/simpson.m
@@ -1,13 +1,13 @@
-function ans = simpson(a,b,n,f,type)
-    h = (b-a)/(n);
+function ans = simpson(a,b,n,f)
+    h = (b-a)/(2*n);
     ans = f(a) + f(b);
+    X = (a:(b-a)/(2*n):b);
+    v=1;
     for i=(1:1:n-1)
-       ans = ans + 2*f(a+(i)*h);
-       p=a+(i)*h;
+       ans = ans + 2*f(X(2*i+v));
     end
     for i=(1:1:n)
-       ans = ans + 4*f(a+(2*i-1)*h/2);
-       p=a+(2*i-1)*h/2;
+       ans = ans + 4*f(X(2*i-1+v));
     end
-    ans = ans * h / 6;
+    ans = ans * h / 3;
 end
