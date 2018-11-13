688499593971c789da0e04bcedd5fee9ad20f218
Andrey K <andreykochnov@yandex.ru>
2018-11-13 21:51:14 +0300

    Update trapezoidal.m

--- HW1/algos/trapezoidal.m
+++ HW1/algos/trapezoidal.m
@@ -1,7 +1,8 @@
 function ans = trapezoidal(a,b,n,f)
     ans = 0;
-    h = (b-a)/(n);
-    for i=(1:1:n)
-       ans = ans + (f(a+(i-1)*h) + f(a+(i)*h))*h/2;
+    X = (a:(b-a)/n:b);
+    v=1;
+    for i=(0:1:n-1)
+       ans = ans + (f(X(i+v)) + f(X(i+1+v)))*(X(i+1+v)-X(i+v))/2;
     end
 end
