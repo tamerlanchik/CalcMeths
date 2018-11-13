a3f1c55fb086dd393307b33445a5cb61d2dfad0b
Ian <andreykochnov@yandex.ru>
2018-11-13 21:47:31 +0300

    Big corrects

--- HW1/algos/riemann_sum.m
+++ HW1/algos/riemann_sum.m
@@ -1,18 +1,19 @@
 function ans = riemann_sum(a,b,n,f,type)
     ans = 0;
-    h = (b-a)/(n);
+    X = (a:(b-a)/n:b);
+    v=1;
     switch type
         case -1
-            for i=(1:1:n)
-               ans = ans + f(a+(i-1)*h)*h;
+            for i=(0:1:n-1)
+               ans = ans + f(X(i+v))*(X(i+1+v)-X(i+v));
             end
         case 0
-            for i=(2:1:n+1)
-               ans = ans + f(a + (h*(2*i-3))/2)*h;
+            for i=(1:1:n)
+               ans = ans + f((X(i-1+v)+X(i+v))/2)*(X(i+v)-X(i-1+v));
             end
         case 1
-            for i=(2:1:n+1)
-               ans = ans + f(a+(i-1)*h)*h; 
+            for i=(1:1:n)
+               ans = ans + f(X(i+v))*(X(i+v)-X(i-1+v)); 
             end
         otherwise
             ans=0;
--- HW1/hw_int.m
+++ HW1/hw_int.m
@@ -5,10 +5,10 @@ addpath("./algos");
 if ~exist("./data")
     mkdir("./data");
 end
-if exist("./data/hw_int_cmd1.txt")
-    delete('./data/hw_int_cmd1.txt');
+if exist("./data/hw_int_cmd.txt")
+    delete('./data/hw_int_cmd.txt');
 end
-diary ./data/hw_int_cmd1.txt;
+diary ./data/hw_int_cmd.txt;
 % -------------------
 
 a = 0.2;
@@ -19,17 +19,23 @@ names = {'riemann_left', 'riemann_mid', 'riemann_right', 'trapezoidal', 'simpson
 N=10;
 d2n=ones(1, length(names));
 D2n=[];
-In = (b-a)*hw_int_func((a+b)/2)*ones(1, length(names));
-teta=[1/3, 1/3, 1/3, 1/3, 1/15, 1/15];
+In = zeros(1, length(names));
+I2n = zeros(1, length(names));
+for j=1:1:6
+   In(j)= hw_int_analog(a,b,N, @hw_int_func, names{j});
+end
+teta=[1/3, 1/3, 1/3, 1/3, 1/15, 1];
 while nnz(d2n>eps)~=0
    N=N*2;
    for j=(1:1:6)
-       I2n = hw_int_analog(a,b,N, @hw_int_func, names{j});
-       d2n(j)=teta(j)*abs(I2n-In(j));
-       In(j)=I2n;
+       I2n(j) = hw_int_analog(a,b,N, @hw_int_func, names{j});
    end
+   d2n = teta.*abs(I2n-In);
+   In(:)=I2n(:);
    D2n = [D2n; d2n];
 end
+N=N/2;
+D2n(size(D2n,1),:) = [];
 
 n=[10,100,1000,N];
 S = zeros(length(n), length(names));
@@ -38,12 +44,11 @@ for i=(1:1:length(n))
        S(i,j) = hw_int_analog(a,b,n(i), @hw_int_func, names{j});
    end
 end
-
-disp(array2table(S, 'VariableNames', names));
-fprintf('N = %d\n', n(length(n)));
-
-diary off;
 %-----------------------------------
+Ns=cell(1,length(n));
+for i=(1:1:length(n))
+    Ns{i} = num2str(n(i));
+end
 
 clf;
 figure(1);
@@ -51,36 +56,52 @@ set(gcf, 'Position', [50, 200, 1500, 600]);
 hold on;
 
 subplot(2,2,1);
-t=(a:(b-a)/N:b)';
-s1=hw_int_func(t);
-plot(t, s1);
+hold on;
+for i=(1:1:4)
+    t=(a:(b-a)/n(i):b)';
+    s1=hw_int_func(t);
+    plot(t, s1);
+end
+
+title("F(x) with several N-s");
 xlabel('X');
 ylabel('Y');
 grid on;
 grid minor;
+legend(Ns);
 
 subplot(2,2,3);
-bar(S, 'Hist');
-ylim([0.214, 0.221]);
+b=bar(S, 'Hist');
+colors=['m', 'b', 'c', 'g', 'k', 'y'];
+for i=1:1:6
+   b(i).FaceColor=colors(i); 
+end
+% ylim([0.210, 0.221]);
 ylabel('Integral value');
 xlabel('N');
-Ns=cell(1,length(n));
-for i=(1:1:length(n))
-    Ns{i} = num2str(n(i));
-end
 xticklabels(Ns);
+title("Plot integral analog");
 grid on;
 grid minor;
+legend(strrep(names, '_', ' '), 'Location', 'eastoutside');
 
 subplot(2,2,[2,4]);
-b=bar(D2n, 'FaceColor', 'flat');
-ylim([0, 0.00001]);
-for k = 1:size(D2n,2)
-    b(k).CData = k;
+hold on;
+for i=(1:1:6)
+   plot(D2n(:,i), 'Color', colors(i)); 
 end
+%ylim([0, 0.0001]);
 xlabel('Number of iteration');
 ylabel(texlabel('Delta2n'));
+title("Plot integral analog");
 grid on;
 grid minor;
-
+title("Delta 2n for all methods");
 legend(strrep(names, '_', ' '));
+
+
+S=[n', S];
+names=['N', names];
+disp(array2table(S, 'VariableNames', names));
+
+diary off;
