34308bc48ab555dcdf0fa4de5425c8947ae7d8ec
Andrey K <andreykochnov@yandex.ru>
2018-11-27 22:11:31 +0300

    Добален цикл

--- Laba4/algos/my_cubic_spline.m
+++ Laba4/algos/my_cubic_spline.m
@@ -4,6 +4,9 @@ function [y, x0] = my_cubic_spline(f1, x1, x0)
     C=zeros(n+1);
     K=zeros(n);
     L=zeros(n);
+    D=zeros(n);
+    B=zeros(n);
+    A=zeros(n);
     for k=(3:1:n)
        Hk = x1(k)-x1(k-1);
        Hkk = x1(k-1)-x1(k-2);
@@ -16,17 +19,19 @@ function [y, x0] = my_cubic_spline(f1, x1, x0)
     for k=(n:-1:2)
        C(k) = K(k) - L(k)*C(k+1);
     end
+    for k=(n:-1:2)
+       Hk = x1(k)-x1(k-1);
+       D(k) = (C(k+1)-C(k))/(3*Hk);
+       B(k) = (f1(k)-f1(k-1))/Hk - C(k)*Hk - D(k)*Hk^2;
+       A(k) = f1(k-1);
+    end
     
     k=2;
     for i=(1:1:length(y))
         if x0(i) > x1(k)
             k = k+1;
         end
-        Hk = x1(k)-x1(k-1);
-        Dk = (C(k+1)-C(k))/(3*Hk);
-        Bk = (f1(k)-f1(k-1))/Hk - C(k)*Hk - Dk*Hk^2;
-        Ak = f1(k-1);
         Hk = x0(i) - x1(k-1);
-        y(i) = Ak + Bk*Hk + C(k)*Hk^2 + Dk*Hk^3;
+        y(i) = A(k) + B(k)*Hk + C(k)*Hk^2 + D(k)*Hk^3;
     end
 end
