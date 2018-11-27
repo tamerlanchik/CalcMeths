7161cb6a2ce802d41ce29ca0ed2575ad8968eca1
Ian <andreykochnov@yandex.ru>
2018-11-27 18:43:39 +0300

    Пункт 2 с заглушками вместо половины формул

--- Laba 5/lab_diff.m
+++ Laba 5/lab_diff.m
@@ -2,13 +2,31 @@ clc;
 clear variables;
 close all force;
 
+
+
 a = 0.2;
 b = 0.7;
-n = 10;
+n = 100;
 accDeg = [1,2,4,6];
-names = ['forward', 'backward', 'central'];
+names = {'forward', 'backward', 'central'};
+u=length(names)
+scalFuncDer = cell(length(accDeg)*length(names), 2);
+temp=1;
+clf;
+%в каждой строке пара векторов - df и t(сетка разбиений)
 for i=1:1:length(accDeg)
     for j=1:1:length(names)
-        
+        [scalFuncDer{temp,:}] = lab_diff_do(a,b,n,names{j}, accDeg(i), @lab_diff_f);
+        temp = temp + 1;
+        figure(temp-1);
+        plot(scalFuncDer{temp-1,:});
+        title(num2str(i)+" " + num2str(j));
     end
 end
+celldisp(scalFuncDer);
+
+clf;
+
+% figure(0);
+% t = (a:(b-a)/(n-1):b);
+% plot(t, lab_diff_df(t));
