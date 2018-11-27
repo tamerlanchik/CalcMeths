a46c3375b961725f7116195523e4449188ffe70a
Ian <andreykochnov@yandex.ru>
2018-11-27 21:05:50 +0300

    Добавлены верные формулы в пункт 2. Код переведен на матрицные операции

--- Laba 5/lab_diff.m
+++ Laba 5/lab_diff.m
@@ -2,10 +2,17 @@ clc;
 clear variables;
 close all force;
 
+% w = [1,2,3,4,5];
+% t=(1:1:10)
+% d = f(t(1:5)).*w
+% 
+% function y = f(x)
+%     y=x^2;
+% end
 
 a = 0.2;
 b = 0.7;
-n = 100;
+n = 10;
 %------------2------------
 accDeg = [1,2,4,6];
 names = {'forward', 'backward', 'central'};
@@ -14,16 +21,17 @@ scalFuncDer = cell(length(accDeg)*length(names), 2);
 temp=1;
 clf;
 %в каждой строке пара векторов - df и t(сетка разбиений)
-for i=1:1:length(accDeg)
-    for j=1:1:length(names)
-        [scalFuncDer{temp,:}] = lab_diff_do(a,b,n,names{j}, accDeg(i), @lab_diff_f);
-        temp = temp + 1;
-%         figure(temp-1);
-%         plot(scalFuncDer{temp-1,:});
-%         title(num2str(i)+" " + num2str(j));
-    end
-end
-celldisp(scalFuncDer);
+% for i=1:1:length(accDeg)
+%     for j=1:1:length(names)
+%         [scalFuncDer{temp,:}] = lab_diff_do(a,b,n,names{j}, accDeg(i), @lab_diff_f);
+%         temp = temp + 1;
+% %         figure(temp-1);
+% %         plot(scalFuncDer{temp-1,:});
+% %         title(num2str(i)+" " + num2str(j));
+%     end
+% end
+% celldisp(scalFuncDer);
+[y,u] = lab_diff_do(a,b,n,names{1}, 1, @lab_diff_f)
 
 %---------3------------
 
--- Laba 5/lab_diff_f.m
+++ Laba 5/lab_diff_f.m
@@ -1,4 +1,4 @@
 function f = lab_diff_f(t)
     w = 2*pi*4;
-    f = 1.16*t + 0.13*sin(w*t) - 0.89*t^2;
+    f = 1.16*t + 0.13*sin(w*t) - 0.89*t.^2;
 end
