a80b860125df9e447151fa4b26cba3d0eaaeebf8
Andrey K <andreykochnov@yandex.ru>
2018-12-11 22:26:50 +0300

    Update lab_diff_do.m

--- Laba 5/lab_diff_do.m
+++ Laba 5/lab_diff_do.m
@@ -1,43 +1,68 @@
 function [t,df] = lab_diff_do(a,b,n,name,k,f)
     h = (b-a)/(n-1);
-    t = (a:h:b)'; %n-1 так как нужно n точек, а не промежутков
+    t = (a:h:b)'; %n-1 С‚Р°Рє РєР°Рє РЅСѓР¶РЅРѕ n С‚РѕС‡РµРє, Р° РЅРµ РїСЂРѕРјРµР¶СѓС‚РєРѕРІ
     df = zeros(n, 1);
 	
-    %----------------------
-    W = cell(4,2);
-    
-    W{1,1} = [1, -1]';
-    W{2,1} = [-1/2, 2, -3/2]';
-    W{3,1} = [ -1/4, 4/3, -3, 4, -25/12 ]';
-    W{4,1} = [-1/6, 6/5, -15/4, 20/3, -15/2, 6, -49/20]';
-    
-    W{2,2} = [1/2, 0, -1/2]';
-    W{3,2} = [-1/12, 2/3, 0, -2/3, 1/12]';
-    W{4,2} = [1/60, -3/20, 3/4, 0, -3/4, 3/20, -1/60]';
-    %-----------------------
-    
-    rows = [1,2,0,3,0,4];
-    %       1 2 3 4 5 6
+    W = my_generator_weights(k,name);
 
     switch name
         case 'forward'
             for i=1:1:n-k
-                df(i) = sum(f(t(i+k:-1:i)).*W{rows(k), 1})/(t(i+1)-t(i));
+                df(i) = sum(f(t(i:1:i+k)).*W)/(t(i+1)-t(i));
             end
             t = t(1:n-k);
             df = df(1:n-k);
         case 'backward'
             for i=k+1:1:n
-                df(i) = sum(f(t(i-k:1:i)).*(-W{rows(k), 1}))/(t(i)-t(i-1));
+                df(i) = sum(f(t(i-k:1:i)).*W)/(t(i)-t(i-1));
             end
             t = t(k+1:end);
             df = df(k+1:end);
         case 'central'
             for i=k/2+1:1:n-k/2
-                df(i) = sum(f(t(i+k/2:-1:i-k/2)).*W{rows(k), 2})/(t(i)-t(i-1));
+                df(i) = sum(f(t(i+k/2:-1:i-k/2)).*W)/(t(i)-t(i-1));
             end
             t = t(k/2+1:n-k/2);
             df = df(k/2+1:n-k/2);
         otherwise
+    end
+end
+
+function W = my_generator_weights(k, name)
+    G = zeros(k+1);
+    G(1,:)=ones(1,k+1);
+    b=zeros(k+1, 1);
+    b(2)=1;
+    switch name
+        case 'forward'
+            y=(0:1:k);
+        case 'backward'
+            y=(-k:1:0);
+        case 'central'
+            y=(k/2:-1:-k/2);
+    end
+    for i=(1:1:k+1)
+        fct=1;
+        for j=2:1:k+1
+            G(j,i) = y(i)^(j-1)/fct;
+            fct = fct*j;
+        end
+    end
+    [W,ok] = my_kramer(G, b);
+end
+
+function [x,ok] = my_kramer(A,b)
+    [n,m] = size(A);
+    d=det(A);
+    x = zeros(n,1);
+    if (n ~= m || d==0)
+        ok=false;
+    else
+        for i=(1:1:n)
+            T = A;
+            T(:,i)=b;
+            x(i) = det(T)/d;
+        end
+        ok=true;
     end
 end
