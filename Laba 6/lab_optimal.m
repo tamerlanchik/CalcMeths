1d2da20e9b35e81230cf04b56d837872d422a9b0
Ian <andreykochnov@yandex.ru>
2018-12-15 20:27:01 +0300

    Add data cell for table

--- Laba 6/lab_optimal.m
+++ Laba 6/lab_optimal.m
@@ -5,12 +5,12 @@ addpath("./algos");
 
 a=-5;
 b=5;
-eps = 10^(-6);
-KMax=10^6;
+eps = 10^(-7);
+KMax=10^4;
 meth_names={'uniform', 'dichotomy', 'golden_ratio'; 'coordinate_descent', 'gradient', 'newton'};
 %------------------------------
 nested_function = @f1;
-mins = cell(6,2);
+data = cell(6,3);
 shapes = ['x', 'o', 'd'];
 figure(1);
 t=(a:eps*2:b);
@@ -18,8 +18,8 @@ plot(t, nested_function(t));
 grid on; grid minor;
 hold on;
 for i=1:1:3
-   [mins{i,1},mins{i,2},k] = optimal_sca(a,b,meth_names{1,i},KMax,eps);
-   plot(mins{i,:}, shapes(i));
+   [data{i,:}] = optimal_sca(a,b,meth_names{1,i},KMax,eps);
+   plot(data{i,1},data{i,2}, shapes(i));
 end
 
 nested_function = @f2;
@@ -33,21 +33,11 @@ alpha 0.5;
 grid on; grid minor;
 hold on;
 for i=1:1:3
-    [x,fMin,k]=optimal_vec(meth_names{2,i},KMax,eps)
-    plot3(x(1),x(2), fMin, [shapes(i),'r']);
+    [data{3+i,:}]=optimal_vec(meth_names{2,i},KMax,eps);
+%     data{3+i,:} = [x,fMin,k];
+    plot3(data{3+i,1}(1),data{3+i,1}(2),data{3+i,2}, [shapes(i),'r']);
 end
 view([10,45]);
 
-% 
-% figure(1);
-% t=(a:eps*2:b);
-% plot(t, nested_function(t));
-% grid on; grid minor;
-% hold on;
-% % [y,x]= uniform_search(a,b,KMax,@nested_function)
-% plot(mins{1,end:-1:1}, 'x');
-% % [y,x]=dichotomy(a,b,KMax,eps,@nested_function);
-% % plot(x, y, 'D', 'color', [146 127 255] / 255);
-% % [y,x]=golden_ratio(a,b,KMax,eps,@nested_function);
-% % plot(x, y, 'o', 'color', [255 198 43] / 255);
+disp(array2table(data));
 
