02794e56a47aadbdf1ddc39f3ff2850a016895b1
Andrey K <andreykochnov@yandex.ru>
2018-12-15 15:13:35 +0300

    Update jacobi.m

--- Laba 5/jacobi.m
+++ Laba 5/jacobi.m
@@ -1,26 +1,30 @@
-function J = jacobi(x0, h, f, name)
+function J = my_jacobi(x0, h, f, name)
     J = zeros(length(f), length(x0));
-    for j=1:1:length(f)
-            switch name
+    switch name
                 case 'forward'
-                    for d=1:1:length(x0)
-                        tX=x0;
-                        tX(d) = x0(d)+h;
-                        J(j,d) = (f{j}(tX) - f{j}(x0))/h;
+                    for j=1:1:length(f)
+                        for d=1:1:length(x0)
+                            tX=x0;
+                            tX(d) = x0(d)+h;
+                            J(j,d) = (f{j}(tX) - f{j}(x0))/h;
+                        end
                     end
                 case 'backward'
-                    for d=1:1:length(x0)
-                        tX=x0;
-                        tX(d) = x0(d)-h;
-                        J(j,d) = (f{j}(x0) - f{j}(tX))/h;
+                    for j=1:1:length(f)
+                        for d=1:1:length(x0)
+                            tX=x0;
+                            tX(d) = x0(d)-h;
+                            J(j,d) = (f{j}(x0) - f{j}(tX))/h;
+                        end
                     end
                 case 'central'
-                    for d=1:1:length(x0)
-                        t1X=x0; t2X=x0;
-                        t1X(d) = x0(d)-h;
-                        t2X(d) = x0(d)+h;
-                        J(j,d) = 0.5*(f{j}(t2X) - f{j}(t1X))/h;
+                    for j=1:1:length(f)
+                        for d=1:1:length(x0)
+                            t1X=x0; t2X=x0;
+                            t1X(d) = x0(d)-h;
+                            t2X(d) = x0(d)+h;
+                            J(j,d) = 0.5*(f{j}(t2X) - f{j}(t1X))/h;
+                        end
                     end
-            end
     end
 end
