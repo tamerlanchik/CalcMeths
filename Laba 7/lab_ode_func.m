ae0bd2814f356f6faef0395545a59cc9fd9fa2f4
Ian <andreykochnov@yandex.ru>
2018-12-22 20:58:56 +0300

    Отдебажил RK4

--- Laba 7/lab_ode_func.m
+++ Laba 7/lab_ode_func.m
@@ -1,4 +1,4 @@
 function dydt = lab_ode_func(t,y)
     m = 1.1; l=1; g=9.81; k=3; u=1;
-    dydt = [y(2), (u-k*l^2-m*g*sin(y(1)))/(m*l^2)];
+    dydt = [y(2); (u-k*y(2)*l^2-m*g*sin(y(1)))/(m*l^2)];
 end
--- Laba 7/lab_ode_rk4.m
+++ Laba 7/lab_ode_rk4.m
@@ -1,12 +1,14 @@
 function X=lab_ode_rk4(t,f,x0)
-    X=zeros(length(t),length(x0));
-    X(1,:) = x0;
+    X=zeros(length(x0),length(t));
+    X(:,1) = x0;
     for k=1:1:length(t)-1
        h  = t(k+1)-t(k);
-       K1 = f(x0,t(k));
-       K2 = f(X(k) + 0.5*h*K1, t(k)+0.5*h);
-       K3 = f(X(k)+0.5*h*K2, t(k)+0.5*h);
-       K4 = f(X(K)+H*k3, T(K)+H);
-       X(k+1) = X(k) + (h/6)*(K1+2*K2+2*K3+K4); 
+       K1 = f(t(k),X(:,k));
+       K2 = f(t(k)+0.5*h,X(:,k) + 0.5*h*K1);
+       K3 = f(t(k)+0.5*h, X(:,k)+0.5*h*K2);
+       K4 = f(t(k)+h, X(:,k)+h*K3);
+%        p=X(k,:)
+       X(:,k+1) = X(:,k) + (h/6)*(K1+2*K2+2*K3+K4) 
     end
+    X=X';
 end
