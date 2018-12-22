452151e40860e5b3b71dc77c42157b39971cbfc0
Ian <andreykochnov@yandex.ru>
2018-12-22 21:17:06 +0300

    Add AB5

--- Laba 7/lab_ode.m
+++ Laba 7/lab_ode.m
@@ -10,15 +10,18 @@ g=9.81;
 k=3;
 u=1;
 
-t=(0:0.1:1);
-x0=[1,1];
+t=(0:0.01:10);
+x0=[3.14/2,0];
 f = @(t,y) -2*y+y^3;
-X1 = lab_ode_rk4(t,@lab_ode_func, x0)
-[t,y] = ode45(@lab_ode_func, t, x0)
-% figure(1);
+X1 = lab_ode_rk4(t,@lab_ode_func, x0);
+X2 = lab_ode_ab5(t,@lab_ode_func, x0);
+[t,y] = ode45(@lab_ode_func, t, x0);
+y=y;
+figure(1);
 % p=X1(:,1)
-% plot(t,X1(:,1));
-% hold on;
-% grid on;
-% grid minor;
-% plot(t,y(1));
+plot(t,X1(:,1));
+hold on;
+grid on;
+grid minor;
+plot(t,X2(:,1));
+plot(t,y(1));
--- Laba 7/lab_ode_rk4.m
+++ Laba 7/lab_ode_rk4.m
@@ -8,7 +8,7 @@ function X=lab_ode_rk4(t,f,x0)
        K3 = f(t(k)+0.5*h, X(:,k)+0.5*h*K2);
        K4 = f(t(k)+h, X(:,k)+h*K3);
 %        p=X(k,:)
-       X(:,k+1) = X(:,k) + (h/6)*(K1+2*K2+2*K3+K4) 
+       X(:,k+1) = X(:,k) + (h/6)*(K1+2*K2+2*K3+K4);
     end
     X=X';
 end
