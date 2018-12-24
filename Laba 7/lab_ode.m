18ae4cbaabe4198defc4fd458a313158878abe14
Ian <andreykochnov@yandex.ru>
2018-12-24 12:13:40 +0300

    Исправили адаптивного РК

--- Laba 7/lab_ode.m
+++ Laba 7/lab_ode.m
@@ -10,25 +10,20 @@ g=9.81;
 k=3;
 u=1;
 
-A=[1,2,3,4];
-b=[4,5,6,7;8,9,10,11]
-i=(A*b')'
-o= b*A'
 
-
-t=(0:0.1:1);
+t=(0:0.1:10);
 x0=[1,1];
 f = @(t,y) -2*y+y^3;
 % X1 = lab_ode_rk4(t,@lab_ode_func, x0)
 % X2 = lab_ode_ab5(t,@lab_ode_func, x0)
-[X3,T] = lab_ode_rk45(0,10,@lab_ode_func,x0);
+[X3,T] = lab_ode_rk45([0,10],@lab_ode_func,x0)
 [t,y] = ode45(@lab_ode_func, t, x0);
 y=y
 figure(1);
 p=size(X3)
 o=size(T)
 % p=X1(:,1)
-plot(T,X3(:,1));
+plot(T,X3);
 hold on;
 grid on;
 grid minor;
--- Laba 7/lab_ode_rk45.m
+++ Laba 7/lab_ode_rk45.m
@@ -1,10 +1,59 @@
-function [X,t]=lab_ode_rk45(tMin,tMax,f,x0)
+function [X,t]=lab_ode_rk45(T,f,x0)
     x0=x0';
-    tMin=tMin
     h0 = 10^(-3);
-    eps=10^(-3);
-    
+    eps=10^(-5);
+    [A,c,b1,b2] = getButcher();
+    h=h0;
+%     while(abs(X1-X2)>eps)
+%             K=zeros(length(x0),6);
+%             for i=1:1:size(K,2)
+%                K(:,i) = f(tMin+h*c(i), x0 + (h*K*A(i,:)')); 
+%             end
+%             p=K*b1'
+%             X1 = x0 + (h*K*b1');
+%             X2 = x0 + (h*K*b2');
+%             h=h/2;
+%             h=h
+%     end
+%     X=zeros(length(x0),(tMax-tMin)/h+1);
+%     X(:,1) = x0;
+%     X(:,2)=X1;
+%     t=(tMin:h:tMax);
+%     for k=2:1:size(X,2)-1
+%         K=zeros(length(x0),6);
+%         for i=1:1:size(K,2)
+%            K(:,i) = f(tMin+h+h*c(i), X(:,k) + (h*K*A(i,:)')); 
+%         end
+%         X(:,k+1) = X(:,k) + (h*K*b1');
+%     end
+    t=[T(1)];
+    X=[x0];
     X1=0; X2=1;
+    while t(end)<=T(2)
+        while(1)
+            K=zeros(length(x0),6);
+            for i=1:1:size(K,2)
+               K(:,i) = f(t(end)+h*c(i), X(:,end) + (h*K*A(i,:)')); 
+            end
+            X1 = X(:,end) + (h*K*b1');
+            X2 = X(:,end) + (h*K*b2');
+            if abs(X1-X2) >= 4*eps
+                h=h/2;
+            elseif abs(X1-X2)<=eps/4
+                h = h*2;
+            else
+                break;
+            end
+%             q = (rand()+2);
+            
+        end
+        t = [t,t(end)+h];
+        X=[X, X1];
+    end
+    X=X';
+end
+
+function [A,c,b1,b2] = getButcher()
     A=zeros(6,6);
     A(2,1)=1/4;
     A(3,1:2)=[3/32, 9/32];
@@ -14,34 +63,4 @@ function [X,t]=lab_ode_rk45(tMin,tMax,f,x0)
     c = [0,1/4,3/8,12/13,1,1/2];
     b1=[16/135,0,6656/12825,28561/56430,-9/50,2/55];
     b2=[25/216,0,1408/2565,2197/4104,-1/5,0];
-    h=h0;
-    x0=x0
-    while(abs(X1-X2)>eps)
-    %         K1 = f(tMin, x0);
-    %         K2 = f(tMin+(1/4)*h, x0+(1/4)*K1);
-    %         K3 = f(tMin+(3/8)*h, x0+(3/32)*K1+(9/32)*K2);
-    %         K4 = f(tMin+(12/13)*h, x0+(1932/2197
-            K=zeros(length(x0),6);
-            for i=1:1:size(K,2)
-               K(:,i) = f(tMin+h*c(i), x0 + (h*K*A(i,:)')); 
-            end
-            p=K*b1'
-            X1 = x0 + (h*K*b1');
-            X2 = x0 + (h*K*b2');
-            h=h/2;
-            h=h
-    end
-%     h=1
-    X=zeros(length(x0),(tMax-tMin)/h+1);
-    X(:,1) = x0;
-    X(:,2)=X1;
-    t=(tMin:h:tMax);
-    for k=2:1:size(X,2)-1
-        K=zeros(length(x0),6);
-        for i=1:1:size(K,2)
-           K(:,i) = f(tMin+h+h*c(i), X(:,k) + (h*K*A(i,:)')); 
-        end
-        X(:,k+1) = X(:,k) + (h*K*b1');
-    end
-    X=X';
 end
