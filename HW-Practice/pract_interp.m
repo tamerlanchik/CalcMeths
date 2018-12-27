133508040c5a7818a98f8bbda489a3ffce066b66
Ian <andreykochnov@yandex.ru>
2018-12-28 01:19:26 +0300

    Реализован bicubic. Генерирует матрицу А

--- HW-Practice/pract_interp.m
+++ HW-Practice/pract_interp.m
@@ -1,11 +1,11 @@
 clc;
 clear variables;
 close all force;
-my_bicubic(1,2,3);
-Na = 5;
-Nb = 8*Na;
-a = [-3,-3];
-b = [3,3];
+%my_bicubic(1,2,3);
+Na = 4;
+Nb = 10*Na;
+a = [-1,-1];
+b = [2,2];
 
 T0 = zeros(Na, 2);
 T0(:,1) = (a(1):(b(1)-a(1))/(Na-1):b(1))';
@@ -17,9 +17,9 @@ T(:,2) = (a(2):(b(2)-a(2))/(Nb-1):b(2))';
 
 f = @(x,y) x.^2 + y.^2 + 2;
 
-[X0,Y0] = meshgrid(T0(:,1),T0(:,2));
-[X,Y] = meshgrid(T(:,1),T(:,2))
-F = my_bilinear(f(X0,Y0), {X0,Y0}, {X,Y})
+[X0,Y0] = meshgrid(T0(:,1),T0(:,2))
+[X,Y] = meshgrid(T(:,1),T(:,2));
+F = my_bicubic(f(X0,Y0), {X0,Y0}, {X,Y});
 figure(1);
 x=size(X)
 y=size(Y)
