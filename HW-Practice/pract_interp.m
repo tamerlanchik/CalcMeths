8abcbe5dc859e037316d90ca5f7e586adf5733b3
Ian <ivanov.vanya.111@mail.ru>
2019-01-21 18:50:08 +0300

    Many graphs

--- HW-Practice/pract_interp.m
+++ HW-Practice/pract_interp.m
@@ -2,10 +2,10 @@ clc;
 clear variables;
 close all force;
 
-Na = 10;
-Nb = 2*Na;
-a = [-4,-4];    %left bottom point
-b = [4,4];      %right top point
+Na = 30;
+Nb = 5*Na;
+a = [-5,-5];    %left bottom point
+b = [5,5];      %right top point
 
 %interpolated grid
 T0 = zeros(Na, 2);
@@ -18,177 +18,161 @@ T(:,1) = (a(1):(b(1)-a(1))/(Nb-1):b(1))';
 T(:,2) = (a(2):(b(2)-a(2))/(Nb-1):b(2))';
 
 %interpolated function
+
 % f = @(x,y) ((x+5).^2 + y.^2 + 10);
-% f = @(x,y) 0.9996*(1090.91 + 4*x*y)*exp(*x*pi/2);
-% f = @(x,y) 1.0316+4*x.^2-2.1*x.^4 + (1/3)*x.^6 + x.*y - 4*y.^2 + 4*y.^4;
-f = @(x,y) 3*((1-x).^2).*exp(-x.^2-(y+1).^2)-10*(x./5-x.^3-y.^5).*exp(-x.^2-y.^2)-(1/3)*exp(-(x+1).^2-y.^2);
 % f = @(x,y) (x.^2 - y.^2)/2;
 % f = @(x,y) x.^2 + 2;
 % f = @(x,y) (x.^2 + y.^2+2).^0.5;
 % f = @(x,y) (5+x)./(5-x).*(x.^2) - y.^2;
-%F'x = 2x
-%F'y = 2y
-%F''xy=0
+
+%from https://www.emse.fr/enbis-emse2009/pdf/articles/Muehlenstaedt%20Kuhnt.pdf
+fList = cell(6,1);
+fNames=cell(6,1);
+%Die force
+fList{1} = @(x,y) 0.9996*(1090.91 + 4*x*y)*exp(x*pi/2);
+fNames{1} = 'Die force';
+
+%Hump function
+fList{2} = @(x,y) 1.0316+4*x.^2-2.1*x.^4 + (1/3)*x.^6 + x.*y - 4*y.^2 + 4*y.^4;
+fNames{2} = 'Hump';
+%Peaks function
+fList{3} = @(x,y) 3*((1-x).^2).*exp(-x.^2-(y+1).^2)-10*(x./5-x.^3-y.^5).*exp(-x.^2-y.^2)-(1/3)*exp(-(x+1).^2-y.^2);
+fNames{3} = 'Peaks';
+%Not smooth
+fList{4} = @(x,y) abs(x.^2 + sin(0.5*pi*y) - y);
+fNames{4} = 'Not smooth';
+%Sibson
+fList{5} = @(x,y) cos(4*pi*((x-0.25).^2 + (y-0.25).^2).^(1/2));
+fNames{5} = 'Sibson';
+
+fList{6}=@(x,y) ((x/20).^2 + (y/20).^2 + 10);
+fNames{6}='Paraboloid';
 
 %grid
 [X0,Y0] = meshgrid(T0(:,1),T0(:,2));
 [X,Y] = meshgrid(T(:,1),T(:,2));
 
-F1 = my_bilinear(f(X0,Y0), {X0,Y0}, {X,Y});
-F2 = my_bicubic(f(X0,Y0), {X0,Y0}, {X,Y});
-
-
-figure(1);
-
-subplot(2,2,1);
-F0 = f(X0,Y0);
-surf(X0,Y0,F0);
-alpha 0.7;
-grid on;
-grid minor;
-
-subplot(2,2,2);
-surf(X,Y,F1);
-alpha 0.7;
-grid on;
-grid minor;
-
-subplot(2,2,3);
-surf(X0,Y0,F0);
-alpha 0.7;
-grid on;
-grid minor;
-
-subplot(2,2,4);
-surf(X,Y,F2);
-% shading interp;
-alpha 0.7;
-grid on;
-grid minor;
-% view([45,45]);
-
-maxM=10;
-Err1=zeros(maxM-2,1);
-Err2=zeros(maxM-2,1);
-Time1=zeros(maxM-2,1);
-Time2=zeros(maxM-2,1);
-Err3=zeros(maxM-2,1);
-Time3=zeros(maxM-2,1);
-CGrid=(3:1:maxM);
-
-i=1;
-for c=CGrid
-   Nb = c*Na; 
-   %interpolated grid
-    T0 = zeros(Na, 2);
-    T0(:,1) = (a(1):(b(1)-a(1))/(Na-1):b(1))';
-    T0(:,2) = (a(2):(b(2)-a(2))/(Na-1):b(2))';
-
-    %interpolation grid
-    T = zeros(Nb, 2);
-    T(:,1) = (a(1):(b(1)-a(1))/(Nb-1):b(1))';
-    T(:,2) = (a(2):(b(2)-a(2))/(Nb-1):b(2))';
-
-    %grid
-    [X0,Y0] = meshgrid(T0(:,1),T0(:,2));
-    [X,Y] = meshgrid(T(:,1),T(:,2));
-    
-    temp1=0;
-    temp2=0;
-    temp3=0;
-    for j=1:1:20
-        tic;
-        F1 = my_bilinear(f(X0,Y0), {X0,Y0}, {X,Y});
-        temp1 = temp1 + toc;
-        
-        tic;
-        F2 = my_bicubic(f(X0,Y0), {X0,Y0}, {X,Y});
-        temp2 = temp2 + toc;
-        
-        tic;
-        F3 = my_optimized_bicubic(f(X0,Y0), {X0,Y0}, {X,Y});
-        temp3 = temp3 + toc;
+meths = { @(A,B,C) my_bilinear(A,B,C); @(A,B,C) my_bicubic(A,B,C); @(A,B,C) my_optimized_bicubic(A,B,C) };
+Err=[];
+Time=[];
+for fN=1:1:size(fList,1)
+    F0 = fList{fN}(X,Y);    %true F value
+    timeAverage = zeros(1,3);
+    F=zeros(size(F0,1),size(F0,2),3);
+    triesNumber=1;
+    for j=1:1:triesNumber
+        for m = 1:1:2
+            tic;
+            F(:,:,m) = meths{m}(fList{fN}(X0,Y0), {X0,Y0}, {X,Y});
+            timeAverage(m) = timeAverage(m) + toc;
+        end
     end
-    Time1(i)=temp1/20;
-    Time2(i)=temp2/20;
-    Time3(i)=temp3/20;
-%     tic;
-%     F2 = my_bicubic(f(X0,Y0), {X0,Y0}, {X,Y});
-%     Time2(c)=toc;
-    err=0;
-    FTrue = f(X,Y);
-    err1 = sum(sum(abs(1-F1./FTrue)));
-    err2 = abs(1-F2./FTrue);
-    temp = isnan(err2);
-    pointsCount=sum(sum(temp~=0));
-    err2(temp)=0;
-    err2 = sum(sum(err2));
-    err3 = abs(1-F3./FTrue);
-    temp = isnan(err3);
-    err3(temp)=0;
-    err3 = sum(sum(err3));
-    err1 = err1/(size(FTrue,1)*size(FTrue,2));
-    err2 = err2/pointsCount;
-    err3 = err3/pointsCount;
-%     fprintf('Normalized: Err1 = %d\nErr2 = %d\n',err1,err2);
-    Err1(i)=err1;
-    Err2(i)=err2;
-    Err3(i)=err3;
-    i=i+1
+    timeAverage = timeAverage./triesNumber;
+    Time=[Time; timeAverage];
+    
+%     figure(fN);
+    figure('NumberTitle', 'off', 'Name', fNames{fN});
+%     name("efe");
+    subplot(3,1,1);
+    surf(X0,Y0,fList{fN}(X0,Y0));
+    alpha 0.7;
+    
+    for j=1:1:size(meths,1)-1
+       subplot(3,1, 1+j);
+       surf(X,Y,F(:,:,j));
+    end
+    
+    F = abs(1-F./F0);
+    temp=isnan(F);
+    pointsCount=sum(sum(temp==0));
+    F(temp)=0;
+    Err = [Err, squeeze(sum(sum(F))./pointsCount)];
 end
-CGrid=CGrid;
-q=size(Err1)
-q=size(Err2)
-q=size(Time1)
-q=size(Time2)
-q=size(CGrid)
-figure(3);
-% subplot(2,3,1);
-% plot(CGrid,Err1, "LineWidth",2);
-% legend("Bilinear");
-% xlabel('Number of division');
-% ylabel('Error per point');
-% 
-% subplot(2,3,4);
-% plot(CGrid,Err2, "LineWidth",2,"Color",'r');
-% hold on;
-% % plot(CGrid,Err3, "LineWidth",2,"Color",'g');
-% legend("Bicubic");
-% xlabel('Number of division');
-% ylabel('Relative error per point');
-subplot(2,3,[1,4]);
-plot(CGrid,Err1, "LineWidth",2);
-hold on;
-plot(CGrid,Err2, "LineWidth",2,"Color",'r');
-legend("Bilinear","Bicubic");
-xlabel('Number of division');
-ylabel('Error per point');
+Err(3,:)=[];
+Time(:,3)=[];
 
-% subplot(2,3,4);
-% plot(CGrid,Err2, "LineWidth",2,"Color",'r');
-% hold on;
-% % plot(CGrid,Err3, "LineWidth",2,"Color",'g');
-% legend("Bicubic");
-% xlabel('Number of division');
-% ylabel('Relative error per point');
-
-subplot(2,3,[2,5]);
-plot(CGrid, Time1,"LineWidth",2);
-hold on;
-plot(CGrid, Time2,"LineWidth",2,"Color",'r');
-% plot(CGrid, Time3,"LineWidth",2,"Color",'g');
-legend("Bilinear", "Bicubic");
-xlabel('Number of division');
-ylabel('Average time, s');
-
-subplot(2,3,3);
-plot(Time1,Err1,"LineWidth",2);
-legend("Bilinear");
+methNames={'Bilinear', 'Bicubic', 'Bicubic optimized'};
+figure('NumberTitle', 'off', 'Name', 'Conclusion: Na=30, Nb=150');
+subplot(1,2,1);
+bar(log10(Err'+1));
+xticklabels(fNames);
+xlabel('Function names');
 ylabel('Relative error per point');
-xlabel('Average time, s');
-Err1=Err1
-subplot(2,3,6);
-plot(Time2,Err2,"LineWidth",2,"Color",'r');
-legend("Bicubic");
+grid on;
+grid minor;
+legend(methNames);
+ylim([0,3]);
+
+subplot(1,2,2);
+bar(Time);
+xticklabels(fNames);
+xlabel('Function names');
+ylabel("Work time, s");
+grid on;
+grid minor;
+legend(methNames);
+
+Na = 5;
+Nb = 30*Na;
+
+%interpolated grid
+T0 = zeros(Na, 2);
+T0(:,1) = (a(1):(b(1)-a(1))/(Na-1):b(1))';
+T0(:,2) = (a(2):(b(2)-a(2))/(Na-1):b(2))';
+
+%interpolation grid
+T = zeros(Nb, 2);
+T(:,1) = (a(1):(b(1)-a(1))/(Nb-1):b(1))';
+T(:,2) = (a(2):(b(2)-a(2))/(Nb-1):b(2))';
+
+
+%grid
+[X0,Y0] = meshgrid(T0(:,1),T0(:,2));
+[X,Y] = meshgrid(T(:,1),T(:,2));
+
+Err=[];
+Time=[];
+for fN=1:1:size(fList,1)
+    F0 = fList{fN}(X,Y);    %true F value
+    timeAverage = zeros(1,3);
+    F=zeros(size(F0,1),size(F0,2),3);
+    triesNumber=1;
+    for j=1:1:triesNumber
+        for m = 1:1:2
+            tic;
+            F(:,:,m) = meths{m}(fList{fN}(X0,Y0), {X0,Y0}, {X,Y});
+            timeAverage(m) = timeAverage(m) + toc;
+        end
+    end
+    timeAverage = timeAverage./triesNumber;
+    Time=[Time; timeAverage];
+    
+    F = abs(1-F./F0);
+    temp=isnan(F);
+    pointsCount=sum(sum(temp==0));
+    F(temp)=0;
+    Err = [Err, squeeze(sum(sum(F))./pointsCount)];
+end
+Err(3,:)=[];
+Time(:,3)=[];
+
+methNames={'Bilinear', 'Bicubic', 'Bicubic optimized'};
+figure('NumberTitle', 'off', 'Name', 'Conclusion: Na=5, Nb=150');
+subplot(1,2,1);
+bar(log10(Err'+1));
+xticklabels(fNames);
+xlabel('Function names');
 ylabel('Relative error per point');
-xlabel('Average time, s');
+grid on;
+grid minor;
+legend(methNames);
+ylim([0,3]);
+
+subplot(1,2,2);
+bar(Time);
+xticklabels(fNames);
+xlabel('Function names');
+ylabel("Work time, s");
+grid on;
+grid minor;
+legend(methNames);
